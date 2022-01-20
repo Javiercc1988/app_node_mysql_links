3ab274859a54bf3745d2a19eba8ca9f3caaced13
Javier <javicallejon88@gmail.com>
2022-01-20 13:33:10 +0100

    Create,edit,delete funcionando

--- routes/links.js
+++ routes/links.js
@@ -1,26 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const pool = require("../database")
-
+const pool = require("../database");
 
 /* GET home page. */
-router.get('/add', function(req, res, next) {
-  res.render("links/add")
+router.get("/add", function (req, res, next) {
+  res.render("links/add");
 });
 
-router.post("/add",async function(req,res,next){
-  
-  const {title,url,description} = req.body
+/* AÑADIR LINK A NUESTRA LISTA */
+router.post("/add", async function (req, res, next) {
+  const { title, url, description } = req.body;
 
   const newLink = {
     title,
     url,
-    description
-  }
+    description,
+  };
 
-  await pool.query("INSERT INTO links SET ?",[newLink])
-  console.log(newLink)
-  res.send("Received")
-})
+  await pool.query("INSERT INTO links SET ?", [newLink]);
+  res.redirect("/links");
+});
+
+/* BORRAR LINK */
+router.get("/delete/:id", async function (req, res, next) {
+  const { id } = req.params;
+
+  await pool.query("DELETE FROM links WHERE id=?;", [id]);
+  res.redirect("/links");
+});
+
+/* LISTAR TODOS LOS LINKS */
+router.get("/", async function (req, res, next) {
+  const links = await pool.query("SELECT * FROM links");
+  res.render("links/list", { links });
+});
+
+/* EDITAR LINK A NUESTRA LISTA */
+router.get("/edit/:id", async function (req, res, next) {
+  const { id } = req.params;
+
+  const links = await pool.query("SELECT * FROM links WHERE id=?;", [id]);
+
+  res.render("links/edit",links[0]);
+});
+
+
+router.post("/edit/:id", async function (req, res, next) {
+
+  const {id} = req.params
+  const { title, url, description } = req.body;
+
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+
+  await pool.query(`UPDATE links SET ? WHERE id=?`, [newLink,id]);
+  res.redirect("/links");
+});
 
 module.exports = router;
